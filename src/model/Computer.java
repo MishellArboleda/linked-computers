92ab9658554514bb88ef12568203f98dca17cde1
Mishell Valeria Arboleda Quintero <1010118618@academico.icesi.edu.co>
2019-04-11 18:28:53 -0500

    start project

---
+++ src/model/Computer.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Computer {
+	
+	private int hd;
+	private int ram;
+	private String ip;
+	
+	public Computer (int h, int r, String ip) {
+		this.hd = h;
+		this.ram = r;
+		this.ip = ip;
+		
+	}
+
+}
